f9b5500adcffb88ad80f1776b50600674d7c3302
Chen Yang <yangc5@mail.uc.edu>
2016-05-20 21:27:07 -0500

    basic theme done

---
+++ app/models/deck.rb
@@ -0,0 +1,9 @@
+class Deck < ActiveRecord::Base
+  validates :name, :presence => true
+  validates :url, :format => {:with => URI::regexp(%w(http https)), :message => "must be a valid URL"}
+  validates :url, :format => {:with => /docs.google.com/, :message => "must be a Google Docs URL"}
+
+  def google_doc_id
+    @google_doc_id ||= url.split("/d/").last.split("/").first.strip
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160520235029) do
+
+  create_table "decks", force: :cascade do |t|
+    t.string   "name"
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
