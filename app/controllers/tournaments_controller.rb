efbde34e615f0ce5889a0f403e1cf2e5b6cf68dd
Chen Yang <yangc5@mail.uc.edu>
2016-07-06 17:11:03 -0500

    tournament new form

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,2 +1,21 @@
 class TournamentsController < ApplicationController
+  def new
+    @tournament = Tournament.new
+  end
+
+  def create
+    @tournament=Tournament.new(tournament_params)
+    if @tournament.save
+      redirect_to @tournament
+    else
+      render :new
+    end
+  end
+
+  private
+    def tournament_params
+      params.require(:tournament).permit(:name)
+    end
+
+
 end
--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -2,4 +2,6 @@ class Tournament < ActiveRecord::Base
   belongs_to :commissioner, :class_name=>"User"
   has_many :tournament_players
   has_many :players, through: :tournament_players, :class_name=>"User"
+
+  validates :name, :presence=>true
 end
